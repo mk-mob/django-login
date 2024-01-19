1148cef76b5a486a7589690f76ab218ad60e76f1
mk-mob <mkmob314@gmail.com>
2024-01-19 23:51:28 +0000

    make login

--- pj_login/urls.py
+++ pj_login/urls.py
@@ -4,13 +4,10 @@ from django.contrib.auth.decorators import login_required
 
 from django.views.generic import TemplateView
 
-# 実はページを表示するだけならこのように1行で書くことが出来ます。
 index_view = TemplateView.as_view(template_name="registration/index.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # login_requiredで囲むとログイン必須のページになります。
     path("", login_required(index_view), name="index"),
     # この１行でdjangoでデフォルトで用意している以下がすべて入ります。
     # ・ログイン
