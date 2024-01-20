3cc51e1ac0c68e9be7392771ffde68ea539d1648
mk-mob <mkmob314@gmail.com>
2024-01-20 09:29:08 +0000

    signup

--- pj_login/settings.py
+++ pj_login/settings.py
@@ -129,3 +129,7 @@ LOGOUT_REDIRECT_URL = "/login/"
 # 暗号化されたhttpsを使うようにする
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
+
+AUTH_USER_MODEL = 'registration.User'
+FRONTEND_URL = "https://localhost-mkmob-login.paiza-user-free.cloud:8000"
+
