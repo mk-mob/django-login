7a60185e8898c4721ee16a56dd9e06ab9393e4fd
mk-mob <71954702+mk-mob@users.noreply.github.com>
2024-01-21 22:11:02 +0900

    Update settings.py

--- pj_login/settings.py
+++ pj_login/settings.py
@@ -72,16 +72,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pj_login.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+import dj_database_url
+from dotenv import (
+    find_dotenv,
+    load_dotenv,
+)
+load_dotenv(find_dotenv())
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':dj_database_url.config(conn_max_age=600)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
