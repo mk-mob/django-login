d1d7a1e7990507326a816ccd18c5d18c1a632efe
mk-mob <mkmob314@gmail.com>
2024-01-21 06:38:53 +0000

    heroku5

--- registration/views.py
+++ registration/views.py
@@ -18,3 +18,14 @@ class ActivateView(TemplateView):
         result = activate_user(uidb64, token)
         # コンテクストのresultにTrue/Falseの結果を渡します。
         return super().get(request, result=result, **kwargs)
+        
+
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import HttpResponseServerError
+
+@requires_csrf_token
+def my_customized_server_error(request, template_name='500.html'):
+    import sys
+    from django.views import debug
+    error_html = debug.technical_500_response(request, *sys.exc_info()).content
+    return HttpResponseServerError(error_html)
